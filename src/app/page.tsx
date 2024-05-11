c2844391468b315a2e2fe98f99dcac5280a6afa5
snju25 <bhandarisanjay879@gmail.com>
2024-05-11 20:04:09 +1000

    fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,6 @@ import Instructors from "@/components/Instructors";
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
-      <h1 className="text-2xl text-center">Chai our code</h1>
       <HeroSection/>
       <FeaturedCourses/>
       {/* not working for  firefox */}
