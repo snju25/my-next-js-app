52fb3cec69e9acb49b548c58be1fa36e0199e3b7
snju25 <bhandarisanjay879@gmail.com>
2024-05-11 20:00:46 +1000

    syntax error for heroSection fixed

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -6,7 +6,7 @@ const page = () => {
   return (
     <div className="bg-[#101523] min-h-screen pt-40 flex flex-col items-center gap-10">
         <p className="text-4xl md:text-7xl font-bold text-white">Contact Us</p>
-        <p className="mx-auto max-w-[500px] text-center text-neutral-400">We're here to help with any questions about our course, programs, or events. Reach out and let us know how we can assist you in your musical journey.</p>
+        <p className="mx-auto max-w-[500px] text-center text-neutral-400">We are here to help with any questions about our course, programs, or events. Reach out and let us know how we can assist you in your musical journey.</p>
         <form  className="flex flex-col gap-10  mt-4 w-[60%]">
           <input type="email" placeholder="Your email address"
           className="rounded-md p-4 bg-neutral-700 w-full"
